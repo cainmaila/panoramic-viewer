30506eab483368d7b7955cce65c01303ba03097c
cain <cainmaila@gmail.com>
2022-01-22 14:37:50 +0800

    save

--- src/controllers/observables/selectAreaObserable.ts
+++ src/controllers/observables/selectAreaObserable.ts
@@ -2,15 +2,6 @@ import { map } from 'rxjs';
 import { Renderer, Vector2 } from 'three';
 import { pointerSelect$, I_Select } from './pointerSelectObservable';
 
-const mapPointerEventToAreaArr = map((pointer: I_Select) => {
-  return [
-    new Vector2(pointer.x, pointer.y),
-    new Vector2(pointer.x1, pointer.y),
-    new Vector2(pointer.x1, pointer.y1),
-    new Vector2(pointer.x, pointer.y1),
-  ];
-});
-
 /**
  * 建立一個範圍選取 Observable
  * @param renderer 要綁定的 renderer
@@ -28,5 +19,14 @@ export const selectAreaObserableByRenderer = (renderer: Renderer) =>
         y1: -(_select.y1 / _clientHeight) * 2 + 1,
       };
     }),
-    mapPointerEventToAreaArr,
+    map((pointer: I_Select) => _toAreaArr(pointer)),
   );
+
+function _toAreaArr(pointer: I_Select): Vector2[] {
+  return [
+    new Vector2(pointer.x, pointer.y),
+    new Vector2(pointer.x1, pointer.y),
+    new Vector2(pointer.x1, pointer.y1),
+    new Vector2(pointer.x, pointer.y1),
+  ];
+}
